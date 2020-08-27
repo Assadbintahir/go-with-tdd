8e0b88b6ef6abe28d1f109a002a2c5b9f8de4185
assadbintahir <asad.u@outlook.com>
2020-08-27 14:40:59 +0500

    Example functions in unit tests and godoc comments

---
+++ integers/adder_test.go
@@ -0,0 +1,46 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	type parameters struct{ first, second int }
+	tests := []struct {
+		name     string
+		params   parameters
+		expected int
+	}{
+		{
+			name:     "should return 4 when first input is 2 and second is 2",
+			params:   parameters{first: 2, second: 2},
+			expected: 4,
+		},
+		{
+			name:     "should return 90 when first input is 50 and second is 40",
+			params:   parameters{first: 50, second: 40},
+			expected: 90,
+		},
+		{
+			name:     "should return 15 when first input is 7 and second is 8",
+			params:   parameters{first: 7, second: 8},
+			expected: 15,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := Adder(test.params.first, test.params.second)
+
+			if got != test.expected {
+				t.Errorf("expected: %d, got: %d", test.expected, got)
+			}
+		})
+	}
+}
+
+func ExampleAdder() {
+	sum := Adder(2, 2)
+	fmt.Println(sum)
+	// Output: 4
+}
